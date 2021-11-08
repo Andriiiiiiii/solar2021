ab3aa5b1d41cfa657f243b5b770be0461134177a
Anton <vsobolev45@gmail.com>
2021-11-08 16:39:52 +0300

    calculate_force should work properly

--- solar_model.py
+++ solar_model.py
@@ -20,8 +20,11 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
-        pass  # FIXME: Взаимодействие объектов
+        # r = max(r, body.R) # FIXME: обработка аномалий при прохождении одного тела сквозь другое
+        force = gravitational_constant * body.m * obj.m / r**2
+        body.Fx += force * (obj.x - body.x) / r  # F cos a
+        body.Fy += force * (obj.y - body.y) / r  # F sin a
+
 
 def move_space_object(body, dt):
     """Перемещает тело в соответствии с действующей на него силой.
