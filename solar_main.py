2e88b010f6661bafecdc8bb80968303fed5096d7
Anton <vsobolev45@gmail.com>
2021-11-15 16:34:53 +0300

    the screen is now 1.2 times bigger than max_distance

--- solar_main.py
+++ solar_main.py
@@ -76,7 +76,7 @@ def open_file():
     model_time = 0.0
     in_filename = "one_satellite.txt"
     space_objects = read_space_objects_data_from_file(in_filename)
-    max_distance = max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
+    max_distance = 1.2*max([max(abs(obj.obj.x), abs(obj.obj.y)) for obj in space_objects])
     calculate_scale_factor(max_distance)
 
 
